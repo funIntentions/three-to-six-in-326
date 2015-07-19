b5c6be36e74dc7a78f496ba89e1d9af3eebb2abc
Dan Russell <danr94@gmail.com>
2015-07-19 00:45:07 -0700

    Added a trigger system to detect when entity gets near another entity.

--- core/src/com/mygdx/projectMeta/systems/TriggerSystem.java
+++ core/src/com/mygdx/projectMeta/systems/TriggerSystem.java
@@ -4,6 +4,9 @@ import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.mygdx.projectMeta.components.*;
 
 /**
@@ -25,6 +28,19 @@ public class TriggerSystem extends IteratingSystem
 
     public void processEntity(Entity entity, float deltaTime)
     {
+        TransformComponent transformComponent = transformMapper.get(entity);
+        TriggerComponent triggerComponent = triggerMapper.get(entity);
 
+        if (triggerComponent.triggerer != null)
+        {
+            TransformComponent triggererTransformComponent = transformMapper.get(triggerComponent.triggerer);
+
+            Vector3 pos1 = transformComponent.position;
+            Vector3 pos2 = triggererTransformComponent.position;
+
+            float distance = Vector3.dst(pos1.x, pos1.y, pos1.z, pos2.x, pos2.y, pos2.z);
+
+            triggerComponent.triggered = (distance >= triggerComponent.range);
+        }
     }
 }
